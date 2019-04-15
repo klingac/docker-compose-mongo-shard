cedf03a5049db0e1c908a69b79e68684d9accefb
klingac <devklingac@gmail.com>
2019-04-15 07:29:44 +0200

    Change default port of config RS instances to 27019

--- scripts/mongo-conf-rs.init.js
+++ scripts/mongo-conf-rs.init.js
@@ -4,9 +4,9 @@ rs.initiate(
      configsvr: true,
      version: 1,
      members: [
-        { _id: 0, host : "mongodb-conf-rs-1:27017" },
-        { _id: 1, host : "mongodb-conf-rs-2:27017" },
-        { _id: 2, host : "mongodb-conf-rs-3:27017" }
+        { _id: 0, host : "mongodb-conf-rs-1:27019" },
+        { _id: 1, host : "mongodb-conf-rs-2:27019" },
+        { _id: 2, host : "mongodb-conf-rs-3:27019" }
      ]
   }
 )
